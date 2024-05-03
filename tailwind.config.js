bd27f1f021e8b3b79e3925df95febfa37714e959
MarkThaValor <geniusmark99@gmail.com>
2024-05-03 18:52:47 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode:'class',
-  content: ["./styles/**/*.{html,js}","./index.html","./request-estimate.html","./scripts/**/*{js}"],
+  content: ["./styles/**/*.{html,js}","./index.html","./request.html"],
   theme: {
     extend: {
       colors:[
