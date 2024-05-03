bbb4e9aaaea14a956d1f3088ea859b053267e497
MarkThaValor <geniusmark99@gmail.com>
2024-05-03 19:26:32 +0100

    first commit

--- scripts/validate.js
+++ scripts/validate.js
@@ -45,16 +45,23 @@ lastname = Id("lastname").value,
 email = Id("email").value,
 location =  Id("location").value,
 target_pest = Id("target_pest").value,
-property_type = Id("property_type"),
+property_type = Id("property_type").value,
 compound = Id("compound").value,
 phone = Id("phone").value,
 date_needed = Id("date_of_service").value,
 time_needed = Id("time_of_service").value;
+added_info = Id("additional_details").value;
 
 
+/*
+Hello Dews and Petals. This is Ogunbiyi Joshua from Lagos. I just requested for an estimate on your website. 
+I have provided the following information: Email - primedrive55@gmail.com, 
+Phone No - 09161940239, Precise Location -  Victoria Island, Target Pest - Rats and coackroaches, 
+Property Description - 3 Bedrooms 1 Living Room, Extra Space - Laundry, Compound - Yes, PFC - , Added Info - Nope. 
+Kindly send the estimate via email when it's ready. Thank you!
+*/
 
-
-        const whatsappURL = `https://api.whatsapp.com/send?phone=2348123916766&text=Hi Ecoshiel fumigation service, My firstname is :${firstname} my %0Alastname is:${lastname}%0AEmail:${email} , I need you service at ${location}`;
+     const whatsappURL = `https://api.whatsapp.com/send?phone=2348123916766&text=Hi Ecoshield fumigation service. This is ${firstname} ${lastname} from ${location}. I just requested for an estimate on your website. I have provided the following information: Email: ${email}, Phone No: ${phone},Target Pest: ${target_pest},Property Description: ${property_type},Compound: ${compound},${(added_info) !=="" ? "Added Info: " + added_info  : ""}Kindly send the estimate via email when it's ready. Thank you!`;
 
       window.location.href = whatsappURL;
 
